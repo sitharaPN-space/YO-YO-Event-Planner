eaf8334d0e14a276690fe754eb24dd2f2eb24e8f
Sithara <sitharapitiyage@gmail.com>
2022-04-20 13:18:27 +0530

    packag.json  file changes

---
+++ build/precache-manifest.3bbb6df73a5a0232732be1ad977dbf96.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e80f9ca1de7bcf553a8055471872eef3",
+    "url": "/index.js/index.html"
+  },
+  {
+    "revision": "677114f576ced4aaaa38",
+    "url": "/index.js/static/css/2.e94d5e4c.chunk.css"
+  },
+  {
+    "revision": "dc853612ac543ecc1ef3",
+    "url": "/index.js/static/css/main.d5d45de4.chunk.css"
+  },
+  {
+    "revision": "677114f576ced4aaaa38",
+    "url": "/index.js/static/js/2.61c0d33e.chunk.js"
+  },
+  {
+    "revision": "bcabce72aec514ca26eb7e657a9fbd6a",
+    "url": "/index.js/static/js/2.61c0d33e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc853612ac543ecc1ef3",
+    "url": "/index.js/static/js/main.ef86b878.chunk.js"
+  },
+  {
+    "revision": "3e2bf20af635a0fe2e7c",
+    "url": "/index.js/static/js/runtime-main.4a328dd1.js"
+  },
+  {
+    "revision": "85064a97348166467bd7bc2ad80ccdce",
+    "url": "/index.js/static/media/404.85064a97.svg"
+  },
+  {
+    "revision": "9ba4b7cbe7f072ea0fe50ec0d4b6c97b",
+    "url": "/index.js/static/media/404.9ba4b7cb.jpg"
+  },
+  {
+    "revision": "192d8f70e8dbebb4b03f98db1a6434c7",
+    "url": "/index.js/static/media/500.192d8f70.jpg"
+  },
+  {
+    "revision": "a0320da1a9ae65d73f6af7780883975c",
+    "url": "/index.js/static/media/500.a0320da1.svg"
+  },
+  {
+    "revision": "6aae84abf0090324eb1daa7eef6bfdd8",
+    "url": "/index.js/static/media/bootstrap-5-logo.6aae84ab.svg"
+  },
+  {
+    "revision": "95abb40d53edf7f917949e8757cdf5c6",
+    "url": "/index.js/static/media/bs5-illustrations.95abb40d.svg"
+  },
+  {
+    "revision": "ed3cd4b507f8209412bc426332f23589",
+    "url": "/index.js/static/media/canada.ed3cd4b5.svg"
+  },
+  {
+    "revision": "c139fe199cc029d2fed4f7641cb3d6dd",
+    "url": "/index.js/static/media/event2.c139fe19.jfif"
+  },
+  {
+    "revision": "935e70467bab0575ad62110dfae45476",
+    "url": "/index.js/static/media/forgot-password.935e7046.jpg"
+  },
+  {
+    "revision": "968aaa24eeffdd779e4fc9c27c5cb730",
+    "url": "/index.js/static/media/france.968aaa24.svg"
+  },
+  {
+    "revision": "d810f62137d64a0a58035c80489b7d54",
+    "url": "/index.js/static/media/germany.d810f621.svg"
+  },
+  {
+    "revision": "612e617f5d72975e63e53d793d757785",
+    "url": "/index.js/static/media/italy.612e617f.svg"
+  },
+  {
+    "revision": "1f905d23af144bc76e488f00d516de2e",
+    "url": "/index.js/static/media/japan.1f905d23.svg"
+  },
+  {
+    "revision": "32442a8e647bf33b852807cd91c492e4",
+    "url": "/index.js/static/media/laravel-logo.32442a8e.svg"
+  },
+  {
+    "revision": "69f2db42fdfeb91a54fe1573b6b102c9",
+    "url": "/index.js/static/media/lock.69f2db42.jpg"
+  },
+  {
+    "revision": "b2d9831a6290a67a05fa6d65bb67d226",
+    "url": "/index.js/static/media/mockup-calendar-presentation.b2d9831a.png"
+  },
+  {
+    "revision": "fc6d4c754fd2b26a2e4775bc91b61bbc",
+    "url": "/index.js/static/media/mockup-map-presentation.fc6d4c75.png"
+  },
+  {
+    "revision": "77a4c6a89e893405dd125243971c3d61",
+    "url": "/index.js/static/media/mockup-presentation.77a4c6a8.png"
+  },
+  {
+    "revision": "00a526136ebe6ca9adbd18a0dcb9064d",
+    "url": "/index.js/static/media/overview.00a52613.jpg"
+  },
+  {
+    "revision": "9992e0ee061c844b089dd94784cf7e9d",
+    "url": "/index.js/static/media/profile-cover.9992e0ee.jpg"
+  },
+  {
+    "revision": "4d26e151b224eaa88df2742351073c56",
+    "url": "/index.js/static/media/profile-picture-1.4d26e151.jpg"
+  },
+  {
+    "revision": "487133c5319652023f459ea4e3a4720a",
+    "url": "/index.js/static/media/profile-picture-2.487133c5.jpg"
+  },
+  {
+    "revision": "9a250e5ca0df93fd467f77c4d7e3d3d4",
+    "url": "/index.js/static/media/profile-picture-3.9a250e5c.jpg"
+  },
+  {
+    "revision": "ccd796b31c6ca0c778c6065b3b6a20b7",
+    "url": "/index.js/static/media/profile-picture-4.ccd796b3.jpg"
+  },
+  {
+    "revision": "baf7bf753ca889c7db833fb0db47abac",
+    "url": "/index.js/static/media/profile-picture-5.baf7bf75.jpg"
+  },
+  {
+    "revision": "9394d800b9cadc76bc0dd4ecb6f9edcf",
+    "url": "/index.js/static/media/react-hero-logo.9394d800.svg"
+  },
+  {
+    "revision": "5e51d949230c7a92d64c73604e5a3243",
+    "url": "/index.js/static/media/react-logo-primary.5e51d949.svg"
+  },
+  {
+    "revision": "f7d45c01a0efd126cc1030d5cf78fd84",
+    "url": "/index.js/static/media/react-logo-transparent.f7d45c01.svg"
+  },
+  {
+    "revision": "b779e4fd2515e28f0233a071f3d46104",
+    "url": "/index.js/static/media/react-logo.b779e4fd.svg"
+  },
+  {
+    "revision": "99d034b6cea27baae946ce783434954f",
+    "url": "/index.js/static/media/react-mockup.99d034b6.png"
+  },
+  {
+    "revision": "0a4cc9cd55c1c298542e559d34622a48",
+    "url": "/index.js/static/media/reset-password.0a4cc9cd.jpg"
+  },
+  {
+    "revision": "43fe41840c256623201cc14f63415614",
+    "url": "/index.js/static/media/settings.43fe4184.jpg"
+  },
+  {
+    "revision": "ea051702c568e9d0199b6ddc1a52fff4",
+    "url": "/index.js/static/media/sign-in.ea051702.jpg"
+  },
+  {
+    "revision": "6774df144d28e4f7c55c1f7725823465",
+    "url": "/index.js/static/media/sign-up.6774df14.jpg"
+  },
+  {
+    "revision": "58ee1d5a0a93d00df3f6c12170ebd3ff",
+    "url": "/index.js/static/media/signin.58ee1d5a.svg"
+  },
+  {
+    "revision": "bbd344a3e9c39621e69a64c02e4b11de",
+    "url": "/index.js/static/media/themesberg-logo.bbd344a3.svg"
+  },
+  {
+    "revision": "105e5059b0997718976601d17fd2a04b",
+    "url": "/index.js/static/media/themesberg.105e5059.svg"
+  },
+  {
+    "revision": "b98aee40114ed3eaae525ce4ec63b0c3",
+    "url": "/index.js/static/media/transactions.b98aee40.jpg"
+  },
+  {
+    "revision": "e5564902e2642c5e6e2e98e68a7d41f5",
+    "url": "/index.js/static/media/united-kingdom.e5564902.svg"
+  },
+  {
+    "revision": "36ab476e5e55f496749ee61897a9cfb5",
+    "url": "/index.js/static/media/united-states-of-america.36ab476e.svg"
+  }
+]);
